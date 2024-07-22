9ee6bdd64b06f3332a6f4c6ae56d3a21232cd2e3
tmull1 <tmull1732@gmail.com>
2024-07-22 14:34:21 -0400

    Initial commit

---
+++ src/HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String friend = "Essence";
+        System.out.println("Hi " + friend + ", how are you?");
+    }
+}
---
+++ src/Vacation.java
@@ -0,0 +1,8 @@
+public class Vacation {
+    public static void main(String[] args) {
+        System.out.println("I would like to visit Paris.");
+        System.out.println("I would like to visit Paris.");
+        System.out.println("I would like to visit Paris.");
+    }
+}
+
