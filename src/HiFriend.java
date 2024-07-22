bffb0cbceabeff052990fa94193743f7e64aeaf3
tmull1 <tmull1732@gmail.com>
2024-07-22 14:37:15 -0400

    Commit

--- src/HiFriend.java
+++ src/HiFriend.java
@@ -1,6 +1,6 @@
 public class HiFriend {
     public static void main(String[] args) {
-        String friend = "Essence";
+        String friend = "Joe";
         System.out.println("Hi " + friend + ", how are you?");
     }
 }
